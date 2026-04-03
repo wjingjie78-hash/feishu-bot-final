c12fe5c5435861109e57f77454484ca531c3e77f
wjingjie78-hash <wjingjie78@gmail.com>
2026-04-03 18:27:13 +0800

    Update main.go

--- api/main.go
+++ api/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"github.com/leochen0511/feishu-chatgpt/pkg/server"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// 专门处理飞书的首次验证请求
+	var data map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&data)
+	
+	if challenge, ok := data["challenge"]; ok {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"challenge\":\"%s\"}", challenge)
+		return
+	}
+	
+	fmt.Fprint(w, "Hello! Bot is running.")
+}
+
 func main() {
-	server.Run()
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
 }
